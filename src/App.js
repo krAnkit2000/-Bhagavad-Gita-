85cfe26b664f77946ca2e8751b6323ed3e40cf0b
ankit <ankitkumar897955@gmail.com>
2024-02-08 20:47:16 +0530

    category component add

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 
 
 import Navbar from './component/Navbar/Nav';
-// import Main from './component/maincontainer/Main';
+import Main from './component/maincontainer/Main';
+import Filter from './component/category/Category';
 
 function App() {
   return (
     <div className="App">
     <Navbar />
+    <Filter/>
+    <Main/>
 
+    
     </div>
   );
 }
