1514f9689fe1b383aa44183cd38b16e44bb3fb96
ankit <ankitkumar897955@gmail.com>
2024-02-08 19:26:06 +0530

    navbar add

---
+++ src/component/Navbar/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import   './Nav.css'
+
+
+
+
+function Nav() {
+  return (
+    <div className='nav'>
+    <a href="/">home</a>
+    <a href="/">about</a>
+    <a href="/main">main</a>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Nav
